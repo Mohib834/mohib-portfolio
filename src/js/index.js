b02ee41d839d4ab5a749708212a5a7cb70c9e48c
Mohib Arshi <mohibarshi834@gmail.com>
2019-06-26 02:53:36 +0530

    smooth scrolling and navbar onscroll design added rotating banner text functionality and deleted last functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,34 @@
 import style from '../styles/main.scss';
-import { elements } from './view/base';
+import { throttle, elements } from './view/base';
 import baffle from 'baffle';
 import 'jquery';
 
-const showDevText = () => {
-    const text = baffle(elements.devText);
-    text.set({
-        characters:'█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░>',
-        speed:120,
-    });
-    text.start();
-    text.reveal(4000);
+const state = {
+    pageIdx: 0,
+    pageBtnTextArr: ['work', 'about', 'contact', 'top'],
 }
 
+const showPage = () => {
+    // if pageidx is not equal to length of section then add 1;
+    state.pageIdx !== elements.sections.length - 1 ? state.pageIdx++ : state.pageIdx = 0;
+    // if pageidx is equal to length of section then add class rotate to pagination btn
+    state.pageIdx === elements.sections.length - 1 ? elements.paginationBtnIcon.classList.add('rotateTop') : elements.paginationBtnIcon.classList.remove('rotateTop');
 
-window.addEventListener('load', showDevText);
+    // For navbar shadow
+    if (state.pageIdx > 0) {
+        elements.nav.classList.add('nav-shadow');
+        elements.navbar.classList.add('navbarBorderRemove');
+    } else if (state.pageIdx === 0) {
+        setTimeout(() => {
+            elements.nav.classList.remove('nav-shadow');
+            elements.navbar.classList.remove('navbarBorderRemove');
+        }, 400);
+    }
+
+    //Changing pagination text
+    elements.paginationBtnText.innerHTML = state.pageBtnTextArr[state.pageIdx];
+    //Smooth scroll
+    elements.sections[state.pageIdx].scrollIntoView({ behavior: 'smooth' });
+}
+
+elements.paginationBtn.addEventListener('click', showPage);
--- src/js/view/base.js
+++ src/js/view/base.js
@@ -1,4 +1,25 @@
-export const elements = {
-    devText:document.querySelector('.dev-text'),
-    fullpage:document.getElementById('fullpage'),
+const elements = {
+    devText: document.querySelector('.dev-text'),
+    sections: document.querySelectorAll('section'),
+    paginationBtn: document.querySelector('.footer-pagination'),
+    paginationBtnText: document.querySelector('.footer-pagination__text'),
+    paginationBtnIcon: document.querySelector('.footer-pagination__icon'),
+    nav: document.querySelector('nav'),
+    navbar: document.querySelector('.navbar'),
+}
+
+function throttle(fn, wait) {
+    var time = Date.now();
+    return function () {
+        if ((time + wait - Date.now()) < 0) {
+            fn();
+            time = Date.now();
+        }
+    }
+}
+
+
+module.exports = {
+    throttle,
+    elements
 }
